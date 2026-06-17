197de960745cf15a9e062a5ebd139fdf46fd6010
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-06-17 17:18:09 -0300

    feat: update authorization to allow all users to make post requests

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -12,7 +12,7 @@ class StorePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,9 @@ class StorePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title'        => ['required', 'string', 'max:255'],
+            'content'      => ['required', 'string'],
+            'scheduled_at' => ['nullable', 'date', 'after:now'],
         ];
     }
 }
